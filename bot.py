c930081db026130fe74090785be0d57f915d9592
Ganesh17032009ramesh <ganeshramesh173@gmail.com>
2025-02-04 18:57:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 import time, os, platform
 from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
+import asyncio
+import re
 
 
 class Bot(Client):
@@ -27,6 +29,7 @@ class Bot(Client):
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
         await super().start()
+        
         if os.path.exists('restart.txt'):
             with open("restart.txt") as file:
                 chat_id, msg_id = map(int, file)
@@ -35,6 +38,7 @@ class Bot(Client):
             except:
                 pass
             os.remove('restart.txt')
+
         temp.BOT = self
         await Media.ensure_indexes()
         me = await self.get_me()
@@ -43,57 +47,37 @@ class Bot(Client):
         temp.B_NAME = me.first_name
         temp.B_LINK = me.mention
         username = '@' + me.username
-        app = web.AppRunner(web_app)
-        await app.setup()
-        await web.TCPSite(app, "0.0.0.0", PORT).start()
+
+        app_runner = web.AppRunner(web_app)
+        await app_runner.setup()
+        await web.TCPSite(app_runner, "0.0.0.0", PORT).start()
+
         try:
             await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
         except:
-            print("Error - Make sure bot admin in LOG_CHANNEL, exiting now")
+            print("Error - Make sure bot is an admin in LOG_CHANNEL, exiting...")
             exit()
+        
         try:
             m = await self.send_message(chat_id=BIN_CHANNEL, text="Test")
             await m.delete()
         except:
-            print("Error - Make sure bot admin in BIN_CHANNEL, exiting now")
+            print("Error - Make sure bot is an admin in BIN_CHANNEL, exiting...")
             exit()
+
         try:
             await self.send_message(chat_id=SUPPORT_GROUP, text=f"<b>{me.mention} Restarted! 🤖</b>")
         except:
-            print("Error - Make sure bot admin in SUPPORT GROUP, exiting now")
-            exit()
-        print(f"\nPyrogram [v{__version__}] Bot [{username}] Started With Python [v{platform.python_version()}]\n")
+            print("Error - Make sure bot is an admin in SUPPORT GROUP, exiting...")
 
+        print(f"\nPyrogram [v{__version__}] Bot [{username}] Started With Python [v{platform.python_version()}]\n")
 
     async def stop(self, *args):
         await super().stop()
         print("Bot Stopped! Bye...")
 
-
     async def iter_messages(self: Client, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
-        """Iterate through a chat sequentially.
-        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-        single call.
-        Parameters:
-            chat_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-            limit (``int``):
-                Identifier of the last message to be returned.
-                
-            offset (``int``, *optional*):
-                Identifier of the first message to be returned.
-                Defaults to 0.
-        Returns:
-            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-        Example:
-            .. code-block:: python
-                async for message in app.iter_messages("pyrogram", 1000, 100):
-                    print(message.text)
-        """
+        """Iterate through a chat sequentially."""
         current = offset
         while True:
             new_diff = min(200, limit - current)
@@ -104,5 +88,12 @@ class Bot(Client):
                 yield message
                 current += 1
 
+
 app = Bot()
-app.run()
+
+async def main():
+    await app.start()
+    await app.idle()  # Keeps the bot running
+    await app.stop()
+
+asyncio.run(main())
